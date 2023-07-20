39341f338af7c717f38188c410deb657183e24a4
lutzlobo <lutzlobo@gmail.com>
2023-07-20 06:44:49 -0300

    Now we can append text on given line number. :D

--- notes.c
+++ notes.c
@@ -1,63 +1,85 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_FILE_SIZE 1024 // The maximum size that a file's content can be
+#define MAX_FILE_SIZE 1024 // Maximum number of lines
 
 int main(int argc, char *argv[]) {
+    // Check if a filename was provided
     if (argc < 2) {
         printf("Please provide a filename as an argument.\n");
         return 1;
     }
 
-    char *filename = argv[1]; // filename is now the first command line argument
+    char *filename = argv[1]; // The first argument is the filename
+    FILE *file; // File pointer
+    char content[MAX_FILE_SIZE][MAX_FILE_SIZE]; // 2D array to hold the content of the file
+    int i = 0; // Counter for the number of lines
 
-    // Declare a file pointer
-    FILE *file;
+    // Open the file in read mode
+    file = fopen(filename, "rt");
 
-    // This will now be an array of character arrays, or an array of strings.
-    // This allows us to store each line of the file as a separate string.
-    char content[MAX_FILE_SIZE][MAX_FILE_SIZE];
-    int i = 0;
-
-    file = fopen(filename, "rt"); // Open the file in read mode
-
+    // Check if the file opened successfully
     if (file == NULL) {
         printf("Could not open file\n");
     }
     else {
-        char line[MAX_FILE_SIZE]; // Buffer to hold each line read from the file
-
-        // Read each line from the file until EOF is reached
-        while ((fgets(line, sizeof(line), file)) != NULL && i < MAX_FILE_SIZE - 1) {
-            strcpy(content[i], line); // Copy each line into the content array
-            i++;
+        // Read the file line by line into the 2D array 'content'
+        while (fgets(content[i], sizeof(content[i]), file) != NULL && i < MAX_FILE_SIZE - 1) {
+            i++; // Increment the line counter
         }
-        printf("\nThe file has %d lines. \n\n", i); //Prints the number of lines on a file
-        content[i][0] = '\0'; // End the final line with a null character
-        fclose(file); // Close the file
+        printf("\nThe file has %d lines. \n\n", i); // Output the number of lines
+
+        // Close the file
+        fclose(file);
     }
-    // Loop through each line of content and print it
+
+    // Print the content of the file
     for (int j = 0; j < i; j++) {
         printf("[%d] %s", j + 1, content[j]);
     }
 
-    printf("\n\nEnter new text to add to the file (or just hit enter to leave the file unchanged): \n");
-    char input[MAX_FILE_SIZE]; // Buffer to hold user input
-    fgets(input, sizeof(input), stdin); // Get user input
-    input[strlen(input) - 1] = '\0'; // Remove the newline character from user input
+    // Ask the user for the line number to append text to
+    printf("\n\nLine number to append: ");
+    int lineNum;
+    scanf("%d", &lineNum);
+    getchar(); // Clear the input buffer
 
-    strcat(content[i-1], input); // Add user input to the last line of content
+    // Ask the user for the text to append
+    printf("Text to append: \n");
+    char appendText[MAX_FILE_SIZE];
+    fgets(appendText, sizeof(appendText), stdin);
 
-    file = fopen(filename, "w"); // Open the file in write mode
+    // Check if the last character is a newline
+    size_t len = strlen(appendText);
+    if (len > 0 && appendText[len - 1] == '\n') {
+        appendText[len - 1] = '\0'; // remove the newline
+    }
+
+    // Check if the line number is valid
+    if (lineNum > 0 && lineNum <= i+1) {
+        // Shift all lines down
+        for (int j = i; j > lineNum; j--) {
+            strcpy(content[j], content[j-1]);
+        }
+        // Replace the next line after selected line with the new text
+        strcpy(content[lineNum], appendText);
+        strcat(content[lineNum], "\n"); // Add newline at the end
+
+        i++; // increment the line count
+    } else {
+        printf("Invalid line number. \n");
+    }
+
+    // Open the file in write mode
+    file = fopen(filename, "w");
     if (file != NULL) {
-        // Loop through each line of content and write it to the file
+        // Write the modified content back to the file
         for (int j = 0; j < i; j++) {
             fputs(content[j], file);
         }
         fclose(file); // Close the file
     } else {
-        printf("Could not open file for writing\n");
+        printf("Error \n");
     }
-    
     return 0;
 }
